0add91c493be5df735f542c5fac425fa643a7d82
nexus0208 <christiandelacruz0208@gmail.com>
2022-01-06 21:59:23 +0800

    Onecall fetch for 7 Days Weather

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ async function getWeatherLocation() {
   const weatherImperial = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=Imperial&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataImperial = await weatherImperial.json();
   console.log(weatherDataImperial);
+
+  const weatherDaily7days = await fetch('http://api.openweathermap.org/data/2.5/onecall?lat=14.5995&lon=120.9842&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456');
+  const weatherDataDaily7days = await weatherDaily7days.json();
+  console.log(weatherDataDaily7days);
 }
 
 getWeatherLocation();
