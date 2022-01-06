1ba5395c4d1db8f82571871a22a1e2cc34e20eae
nexus0208 <christiandelacruz0208@gmail.com>
2022-01-06 21:22:59 +0800

    Successful fetch: Manila Weather

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function getWeatherLocation() {
-  const weather = await fetch('https://api.openweathermap.org/data/2.5/weather?q=London&appid={c205a33f23c3e0dd3c6166231519e456}');
+  const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherData = await weather.json();
   console.log(weatherData);
 }
