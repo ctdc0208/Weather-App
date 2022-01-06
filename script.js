f231cf36f78f5713e7f6494fa1ef3e6f0a504435
nexus0208 <christiandelacruz0208@gmail.com>
2022-01-06 22:30:01 +0800

    Return temp only

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+const temp = document.querySelector('temp');
+
+
+
 async function getWeatherLocation() {
   const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherData = await weather.json();
+  /*temp.textContent = weatherData.main.temp;*/
+
   console.log(weatherData);
 
-  const weatherMetric = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=metric&APPID=c205a33f23c3e0dd3c6166231519e456');
-  const weatherDataMetric = await weatherMetric.json();
-  console.log(weatherDataMetric);
 
+  const weatherMetric = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&units=metric&APPID=c205a33f23c3e0dd3c6166231519e456');
+  const weatherDataMetric = await weatherMetric.json();
+  temp.textContent = weatherDataMetric.main.temp;
+  console.log(weatherDataMetric);
+/*
   const weatherImperial = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=Imperial&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataImperial = await weatherImperial.json();
   console.log(weatherDataImperial);
@@ -14,6 +22,7 @@ async function getWeatherLocation() {
   const weatherDaily7days = await fetch('http://api.openweathermap.org/data/2.5/onecall?lat=14.5995&lon=120.9842&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataDaily7days = await weatherDaily7days.json();
   console.log(weatherDataDaily7days);
+  */
 }
 
 getWeatherLocation();
