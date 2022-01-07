33a60d76f9415e3d57131df6f6937bd0b501060e
nexus0208 <christiandelacruz0208@gmail.com>
2022-01-07 23:25:23 +0800

    Display the returned information

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const temp = document.querySelector('temp');
+const nameCountry = document.querySelector('.name_country');
+const dateTime = document.querySelector('.date_time');
+const temp = document.querySelector('.temp');
+const unit = document.querySelector('.unit');
+const clouds = document.querySelector('.clouds');
+const feelsLike = document.querySelector('.feels_like');
+const humidity = document.querySelector('.humidity');
+const wind = document.querySelector('.wind');
+
 
 
 
 async function getWeatherLocation() {
+/*
   const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherData = await weather.json();
-  /*temp.textContent = weatherData.main.temp;*/
+  temp.textContent = weatherData.main.temp;
 
   console.log(weatherData);
-
+*/
 
   const weatherMetric = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&units=metric&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataMetric = await weatherMetric.json();
+
+  nameCountry.textContent = weatherDataMetric.name;
   temp.textContent = weatherDataMetric.main.temp;
+  unit.textContent = "Metric";
+  clouds.textContent = weatherDataMetric.weather[0].description;
+  feelsLike.textContent = weatherDataMetric.main.feels_like;
+  humidity.textContent = weatherDataMetric.main.humidity;
+  wind.textContent = weatherDataMetric.wind.speed;
+
   console.log(weatherDataMetric);
+
 /*
   const weatherImperial = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=Imperial&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataImperial = await weatherImperial.json();
@@ -22,7 +40,8 @@ async function getWeatherLocation() {
   const weatherDaily7days = await fetch('http://api.openweathermap.org/data/2.5/onecall?lat=14.5995&lon=120.9842&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherDataDaily7days = await weatherDaily7days.json();
   console.log(weatherDataDaily7days);
-  */
+*/
+
 }
 
 getWeatherLocation();
