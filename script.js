a50a922f61dd6bbf55124abd5699eeccb9f9c7db
nexus0208 <christiandelacruz0208@gmail.com>
2022-01-13 23:11:20 +0800

    add function that input location and fetch, show its data

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const dateTime = document.querySelector('.date_time');
 const clouds = document.querySelector('.clouds');
 const temp = document.querySelector('.temp');
 const unit = document.querySelector('.unit');
+const searchBox = document.querySelector('.search-location-input');
+const searchIcon = document.querySelector('.search-location-button');
 
 // upper left content div's
 const feelsLike = document.querySelector('.feels_like');
@@ -47,7 +49,17 @@ const daily_6_high_temp = document.querySelector('.daily_6_high_temp');
 const daily_6_low_temp = document.querySelector('.daily_6_low_temp');
 const daily_6_clouds = document.querySelector('.daily_6_clouds');
 
-let cityName = 'Manila';
+let lastCity = 'Manila';
+
+function getCityName() {
+  const input = document.querySelector('.search-location-input');
+  const cityName = input.value;
+
+  if (cityName) {
+    return cityName;
+  }
+  return '';
+}
 
 /*
 function getCityName() {
@@ -74,7 +86,7 @@ function buildUrlDaily(latitude, longitude) {
   return `http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}=&lon=${longitude}&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456`;
 }
 */
-async function getWeatherLocation() {
+async function getWeatherData(initialLoad = false) {
 /*
   const weather = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Manila&APPID=c205a33f23c3e0dd3c6166231519e456');
   const weatherData = await weather.json();
@@ -86,76 +98,109 @@ async function getWeatherLocation() {
   const weatherDataMetric = await weatherMetric.json();
 
 */
+  try {
+    let cityName;
+    // default weather location on initial load
+    if (initialLoad) {
+      cityName = 'Manila';
+    } else {
+      // if not initial load, get relevent weather data
+      cityName = getCityName();
+    }
+
+    // if no name entered, exit function
+    if (!cityName) {
+      return;
+    }
+
+    
+    lastCity = cityName;
 
 
-// API for Astronomical API (Latitude and Longitude, Time)
-  const weatherAstronomical = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=c205a33f23c3e0dd3c6166231519e456`);
-  const weatherAstronomicalData = await weatherAstronomical.json();
 
-  const latitude = weatherAstronomicalData.coord.lat;
-  const longitude = weatherAstronomicalData.coord.lon;
+    // API for Astronomical API (Latitude and Longitude, Time)
+      const weatherAstronomical = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=c205a33f23c3e0dd3c6166231519e456`);
+      const weatherAstronomicalData = await weatherAstronomical.json();
 
-  const getDate = await fetch(`https://api.ipgeolocation.io/astronomy?apiKey=b501d43a8ae843deb4959fbbd33fc521&location=${cityName}`);
-  const getDateDate = await getDate.json();
+      const latitude = weatherAstronomicalData.coord.lat;
+      const longitude = weatherAstronomicalData.coord.lon;
 
-/*
-  const weatherImperial = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=Imperial&APPID=c205a33f23c3e0dd3c6166231519e456');
-  const weatherDataImperial = await weatherImperial.json();
-  console.log(weatherDataImperial);
-*/
+      const getDate = await fetch(`https://api.ipgeolocation.io/astronomy?apiKey=b501d43a8ae843deb4959fbbd33fc521&location=${cityName}`);
+      const getDateDate = await getDate.json();
 
-  const weatherDaily7days = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456`);
-  const weatherDataDaily7days = await weatherDaily7days.json();
-  console.log(weatherDataDaily7days);
+    /*
+      const weatherImperial = await fetch('http://api.openweathermap.org/data/2.5/find?q=Manila&units=Imperial&APPID=c205a33f23c3e0dd3c6166231519e456');
+      const weatherDataImperial = await weatherImperial.json();
+      console.log(weatherDataImperial);
+    */
 
-  // upper right contents
-  nameLocation.textContent = weatherAstronomicalData.name;
-  country.textContent = weatherAstronomicalData.sys.country;
-  dateTime.textContent = getDateDate.date + ' ' + getDateDate.current_time;
-  clouds.textContent = weatherDataDaily7days.current.weather[0].description;
-  temp.textContent = weatherDataDaily7days.current.temp + ' °C' ;
-  // unit
+      const weatherDaily7days = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=metric&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456`);
+      const weatherDataDaily7days = await weatherDaily7days.json();
+      console.log(weatherDataDaily7days);
 
-  // upper left contents
-  feelsLike.textContent = 'Feels Like : ' + weatherDataDaily7days.current.feels_like + ' °C';
-  humidity.textContent = 'Humidity : ' + weatherDataDaily7days.current.humidity + ' %';
-  wind.textContent = 'Wind Speed : ' + weatherDataDaily7days.current.wind_speed + ' km/h';
+      // upper right contents
+      nameLocation.textContent = weatherAstronomicalData.name;
+      country.textContent = weatherAstronomicalData.sys.country;
+      dateTime.textContent = getDateDate.date + ' ' + getDateDate.current_time;
+      clouds.textContent = weatherDataDaily7days.current.weather[0].description;
+      temp.textContent = weatherDataDaily7days.current.temp + ' °C' ;
+      // unit
 
-  // bottom contents
-  daily_0_day.textContent = "0 day";
-  daily_0_high_temp.textContent = weatherDataDaily7days.daily[0].temp.max + ' °C';
-  daily_0_low_temp.textContent = weatherDataDaily7days.daily[0].temp.min + ' °C';
-  daily_0_clouds.textContent = weatherDataDaily7days.daily[0].weather[0].description;
+      // upper left contents
+      feelsLike.textContent = 'Feels Like : ' + weatherDataDaily7days.current.feels_like + ' °C';
+      humidity.textContent = 'Humidity : ' + weatherDataDaily7days.current.humidity + ' %';
+      wind.textContent = 'Wind Speed : ' + weatherDataDaily7days.current.wind_speed + ' km/h';
 
-  daily_1_day.textContent = "1 day";
-  daily_1_high_temp.textContent = weatherDataDaily7days.daily[1].temp.max + ' °C';
-  daily_1_low_temp.textContent = weatherDataDaily7days.daily[1].temp.min + ' °C';
-  daily_1_clouds.textContent = weatherDataDaily7days.daily[1].weather[0].description;
+      // bottom contents
+      daily_0_day.textContent = "0 day";
+      daily_0_high_temp.textContent = weatherDataDaily7days.daily[0].temp.max + ' °C';
+      daily_0_low_temp.textContent = weatherDataDaily7days.daily[0].temp.min + ' °C';
+      daily_0_clouds.textContent = weatherDataDaily7days.daily[0].weather[0].description;
 
-  daily_2_day.textContent = "2 day";
-  daily_2_high_temp.textContent = weatherDataDaily7days.daily[2].temp.max + ' °C';
-  daily_2_low_temp.textContent = weatherDataDaily7days.daily[2].temp.min + ' °C';
-  daily_2_clouds.textContent = weatherDataDaily7days.daily[2].weather[0].description;
+      daily_1_day.textContent = "1 day";
+      daily_1_high_temp.textContent = weatherDataDaily7days.daily[1].temp.max + ' °C';
+      daily_1_low_temp.textContent = weatherDataDaily7days.daily[1].temp.min + ' °C';
+      daily_1_clouds.textContent = weatherDataDaily7days.daily[1].weather[0].description;
 
-  daily_3_day.textContent = "3 day";
-  daily_3_high_temp.textContent = weatherDataDaily7days.daily[3].temp.max + ' °C';
-  daily_3_low_temp.textContent = weatherDataDaily7days.daily[3].temp.min + ' °C';
-  daily_3_clouds.textContent = weatherDataDaily7days.daily[3].weather[0].description;
+      daily_2_day.textContent = "2 day";
+      daily_2_high_temp.textContent = weatherDataDaily7days.daily[2].temp.max + ' °C';
+      daily_2_low_temp.textContent = weatherDataDaily7days.daily[2].temp.min + ' °C';
+      daily_2_clouds.textContent = weatherDataDaily7days.daily[2].weather[0].description;
 
-  daily_4_day.textContent = "4 day";
-  daily_4_high_temp.textContent = weatherDataDaily7days.daily[4].temp.max + ' °C';
-  daily_4_low_temp.textContent = weatherDataDaily7days.daily[4].temp.min + ' °C';
-  daily_4_clouds.textContent = weatherDataDaily7days.daily[4].weather[0].description;
+      daily_3_day.textContent = "3 day";
+      daily_3_high_temp.textContent = weatherDataDaily7days.daily[3].temp.max + ' °C';
+      daily_3_low_temp.textContent = weatherDataDaily7days.daily[3].temp.min + ' °C';
+      daily_3_clouds.textContent = weatherDataDaily7days.daily[3].weather[0].description;
 
-  daily_5_day.textContent = "5 day";
-  daily_5_high_temp.textContent = weatherDataDaily7days.daily[5].temp.max + ' °C';
-  daily_5_low_temp.textContent = weatherDataDaily7days.daily[5].temp.min + ' °C';
-  daily_5_clouds.textContent = weatherDataDaily7days.daily[5].weather[0].description;
+      daily_4_day.textContent = "4 day";
+      daily_4_high_temp.textContent = weatherDataDaily7days.daily[4].temp.max + ' °C';
+      daily_4_low_temp.textContent = weatherDataDaily7days.daily[4].temp.min + ' °C';
+      daily_4_clouds.textContent = weatherDataDaily7days.daily[4].weather[0].description;
 
-  daily_6_day.textContent = "6 day";
-  daily_6_high_temp.textContent = weatherDataDaily7days.daily[6].temp.max + ' °C';
-  daily_6_low_temp.textContent = weatherDataDaily7days.daily[6].temp.min + ' °C';
-  daily_6_clouds.textContent = weatherDataDaily7days.daily[6].weather[0].description;
-}
+      daily_5_day.textContent = "5 day";
+      daily_5_high_temp.textContent = weatherDataDaily7days.daily[5].temp.max + ' °C';
+      daily_5_low_temp.textContent = weatherDataDaily7days.daily[5].temp.min + ' °C';
+      daily_5_clouds.textContent = weatherDataDaily7days.daily[5].weather[0].description;
 
-getWeatherLocation();
+      daily_6_day.textContent = "6 day";
+      daily_6_high_temp.textContent = weatherDataDaily7days.daily[6].temp.max + ' °C';
+      daily_6_low_temp.textContent = weatherDataDaily7days.daily[6].temp.min + ' °C';
+      daily_6_clouds.textContent = weatherDataDaily7days.daily[6].weather[0].description;
+
+  } catch (err) {
+    console.log("error cityName");
+  }
+};
+
+
+searchIcon.addEventListener('click', () => {
+  getWeatherData();
+});
+
+searchBox.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    getWeatherData();
+  }
+});
+
+getWeatherData();
