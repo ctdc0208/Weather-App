08a435f2b9150d683c0443ae5e77b221d00ab296
nexus0208 <christiandelacruz0208@gmail.com>
2023-06-02 09:03:09 +0800

    add cloud icons

--- script.js
+++ script.js
@@ -50,6 +50,18 @@ async function getWeatherData(unit, unitTemp, unitSpeed, initialLoad = false) {
       const weatherDaily7days = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=${unit}&exclude=hourly,minutely&APPID=c205a33f23c3e0dd3c6166231519e456`);
       const weatherDataDaily7days = await weatherDaily7days.json();
       console.log(weatherDataDaily7days)
+      
+      const iconUrlArray = [];
+
+      for (let i = 0; i < 7; i++) {
+        const iconCode = weatherDataDaily7days.daily[i].weather[0].icon;
+        const iconUrl = `http://openweathermap.org/img/w/${iconCode}.png`;
+        iconUrlArray.push(iconUrl);
+        console.log(iconUrl);
+      }
+      
+      console.log(iconUrlArray);
+
 
       const dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
       const dayNamesArray = [];
@@ -73,6 +85,7 @@ async function getWeatherData(unit, unitTemp, unitSpeed, initialLoad = false) {
       document.querySelector('.date').textContent = currentDate;
       document.querySelector('.day').textContent = dayNamesArray[0];
       document.querySelector('.time').textContent = currentTime;
+      document.getElementById('current-cloud').src = iconUrlArray[0]
       document.querySelector('.clouds').textContent = weatherDataDaily7days.current.weather[0].description;
       document.querySelector('.temp').textContent = weatherDataDaily7days.current.temp + `${unitTemp}`;
 
@@ -85,32 +98,32 @@ async function getWeatherData(unit, unitTemp, unitSpeed, initialLoad = false) {
       document.querySelector('.daily_1_day').textContent = dayNamesArray[1];
       document.querySelector('.daily_1_high_temp').textContent = weatherDataDaily7days.daily[1].temp.max + `${unitTemp}`;
       document.querySelector('.daily_1_low_temp').textContent = weatherDataDaily7days.daily[1].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_1_clouds').textContent = weatherDataDaily7days.daily[1].weather[0].description;
+      document.getElementById('daily_1_clouds').src = iconUrlArray[1]
 
       document.querySelector('.daily_2_day').textContent = dayNamesArray[2];
       document.querySelector('.daily_2_high_temp').textContent = weatherDataDaily7days.daily[2].temp.max + `${unitTemp}`;
       document.querySelector('.daily_2_low_temp').textContent = weatherDataDaily7days.daily[2].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_2_clouds').textContent = weatherDataDaily7days.daily[2].weather[0].description;
+      document.getElementById('daily_2_clouds').src = iconUrlArray[2]
 
       document.querySelector('.daily_3_day').textContent = dayNamesArray[3];
       document.querySelector('.daily_3_high_temp').textContent = weatherDataDaily7days.daily[3].temp.max + `${unitTemp}`;
       document.querySelector('.daily_3_low_temp').textContent = weatherDataDaily7days.daily[3].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_3_clouds').textContent = weatherDataDaily7days.daily[3].weather[0].description;
+      document.getElementById('daily_3_clouds').src = iconUrlArray[3]
 
       document.querySelector('.daily_4_day').textContent = dayNamesArray[4];
       document.querySelector('.daily_4_high_temp').textContent = weatherDataDaily7days.daily[4].temp.max + `${unitTemp}`;
       document.querySelector('.daily_4_low_temp').textContent = weatherDataDaily7days.daily[4].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_4_clouds').textContent = weatherDataDaily7days.daily[4].weather[0].description;
+      document.getElementById('daily_4_clouds').src = iconUrlArray[4]
 
       document.querySelector('.daily_5_day').textContent = dayNamesArray[5];
       document.querySelector('.daily_5_high_temp').textContent = weatherDataDaily7days.daily[5].temp.max + `${unitTemp}`;
       document.querySelector('.daily_5_low_temp').textContent = weatherDataDaily7days.daily[5].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_5_clouds').textContent = weatherDataDaily7days.daily[5].weather[0].description;
+      document.getElementById('daily_5_clouds').src = iconUrlArray[5]
 
       document.querySelector('.daily_6_day').textContent = dayNamesArray[6];
       document.querySelector('.daily_6_high_temp').textContent = weatherDataDaily7days.daily[6].temp.max + `${unitTemp}`;
       document.querySelector('.daily_6_low_temp').textContent = weatherDataDaily7days.daily[6].temp.min + `${unitTemp}`;
-      document.querySelector('.daily_6_clouds').textContent = weatherDataDaily7days.daily[6].weather[0].description;
+      document.getElementById('daily_6_clouds').src = iconUrlArray[6]
 
   } catch (err) {
     // error searching location message
